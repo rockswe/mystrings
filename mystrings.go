59a5c774d80a14b5879ee12574d13d51b6bc5ab0
Efe Kaya <sithban2@gmail.com>
2023-12-29 18:33:55 -0700

    Create mystrings.go

---
+++ mystrings.go
@@ -0,0 +1,15 @@
+package mystrings
+
+// Need to capitalize first letter of the function
+
+func Reverse(s string) string {
+	result := ""
+	for _, v := range s {
+		result = string(v) + result
+	}
+	return result
+}
+
+func main() {
+
+}
